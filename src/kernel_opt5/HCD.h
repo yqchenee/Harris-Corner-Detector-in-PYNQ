72a4f2feca8cb0d70f18cd3b08d24d04812002f7
yqchenee <a6330610@gmail.com>
2021-09-25 11:06:22 +0800

    opt5: vector N = 2 pass, with exceed BRAM

---
+++ src/kernel_opt5/HCD.h
@@ -0,0 +1,35 @@
+#ifndef _HCD_H_
+#define _HCD_H_
+#include <ap_axi_sdata.h>
+#include <hls_stream.h>
+#include <hls_vector.h>
+#include <ap_fixed.h>
+#include "ap_video.h"
+
+#define MAX_WIDTH 1920
+#define MAX_HEIGHT 1080
+
+typedef unsigned int uint32_t;
+
+// top function input, output
+// data[0:7]:G, data[8:15]:B, data[16:23]:R
+#define N 2
+#define v_ind(i) (i)/N
+#define v_ind_ind(i) (i)%N
+typedef ap_int<32> PIXEL;
+typedef hls::vector<PIXEL, N> PIXEL_vec;
+typedef hls::stream<PIXEL_vec> stream_t;
+
+
+// typedef ap_window<PIXEL_vec,3,3> WINDOW;
+typedef ap_linebuffer<PIXEL_vec, 3, MAX_WIDTH/N> BUFFER_3;
+typedef ap_linebuffer<PIXEL_vec, 5, MAX_WIDTH/N> BUFFER_5;
+
+void HCD(
+    stream_t* pstrmInput,
+    stream_t* pstrmOutput,
+    int row,
+    int col
+);
+
+#endif
