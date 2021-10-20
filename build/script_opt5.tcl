e878e96b6b5dde0cd62d2237c4ab3ca957ac6739
yqchenee <a6330610@gmail.com>
2021-10-20 14:13:42 +0800

    script opt5

---
+++ build/script_opt5.tcl
@@ -0,0 +1,20 @@
+############################################################
+## This file is generated automatically by Vitis HLS.
+## Please DO NOT edit it.
+## Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project opt5
+set_top HCD
+add_files src/kernel_opt5/HCD.cpp
+add_files src/kernel_opt5/HCD.h
+add_files src/kernel_opt5/ap_video.h
+add_files -tb src/kernel_opt5/HCDtest.cpp
+open_solution "solution1" -flow_target vivado
+set_part {xc7z020clg484-1}
+create_clock -period 10 -name default
+set_clock_uncertainty 2
+#source "./opt5/solution1/directives.tcl"
+csim_design
+csynth_design
+# cosim_design -trace_level all
+# export_design -format ip_catalog
